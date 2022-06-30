33866a400312427803f59d98458ba363bfe2ff8e
Olivia Ganly <oganly@icloud.com>
2022-06-30 12:28:04 -0400

    extinction done

--- finalProj/ViewController.swift
+++ finalProj/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func link1(_ sender: Any) {
+        
+    }
+    
 }
 
